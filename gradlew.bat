b5235234d7d908c9dfb8c9dbeaf9591507ecd8f1
MarekDesignLeaf <marek@designleaf.co.uk>
2026-02-27 08:38:42 +0000

    Add gradlew.bat

---
+++ gradlew.bat
@@ -0,0 +1,12 @@
+@rem Gradle startup script for Windows
+@if "%DEBUG%"=="" @echo off
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+set APP_HOME=%~dp0
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+@rem Execute Gradle
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% ^
+  "-Dorg.gradle.appname=%APP_BASE_NAME%" ^
+  -classpath "%CLASSPATH%" ^
+  org.gradle.wrapper.GradleWrapperMain %*
